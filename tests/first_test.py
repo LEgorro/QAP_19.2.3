7bca2b7f92a009bccad15ef7aac135c4916d5c2b
LEgorro <87496624+LEgorro@users.noreply.github.com>
2021-10-12 21:31:22 +0300

    Add files

---
+++ tests/first_test.py
@@ -0,0 +1,22 @@
+from app.calculator import Calculator
+import pytest
+
+
+from app.calculator import Calculator
+
+
+class TestCalc:
+   def setup(self):
+       self.calc = Calculator
+
+   def test_multiply_calculate_correctly(self):
+       assert self.calc.multiply(self, 5, 5) == 25
+
+   def test_division_calculate_correctly(self):
+       assert self.calc.division(self, 4, 2) == 2
+
+   def test_subtraction_calculate_correctly(self):
+       assert self.calc.subtraction(self, 10, 2) == 8
+
+   def test_subtraction_adding_correctly(self):
+       assert self.calc.adding(self, 15, 3) == 18
